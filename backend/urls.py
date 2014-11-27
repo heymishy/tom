53c81e42a5fd3b01124e267eb96fa7e4434c967d
Hamish King <heymishy@gmail.com>
2014-11-27 23:22:32 +0000

    fixed urls to be use {% url 'name' instead of harcoding - solves project id issues

--- backend/urls.py
+++ backend/urls.py
@@ -6,11 +6,11 @@ urlpatterns = patterns('',
 	#url(r'^functions/$', views.FunctionList.as_view(), name='functions'),
 	url(r'^projects/(?P<function_project>\d+)/functions$', views.FunctionList.as_view(), name='function_list'),
 	# /backend/1/capabilities
-	url(r'^projects/(?P<capability_project>\d+)/capabilities/$', views.CapabilityList.as_view(), name='capability_list'),
+	url(r'^projects/(?P<capability_project>\d+)/capabilities/$', views.CapabilityList.as_view(), name='capability-list'),
 	# /backend/capabilities/add - Add capabilities
-	url(r'^projects/(?P<capability_project>\d+)/capabilities/add/$', views.Add_Capability.as_view(), name='add_capability'),
+	url(r'^projects/(?P<capability_project>\d+)/capabilities/add/$', views.Add_Capability.as_view(), name='add-capability'),
 	# /functions/1
-	url(r'^projects/(?P<pk>\d+)/functions/(?P<function_id>\d+)/$', views.FunctionDetail.as_view(), name='function_detail'),
+	url(r'^projects/(?P<pk>\d+)/functions/(?P<function_id>\d+)/$', views.FunctionDetail.as_view(), name='function-detail'),
 	# /backend/projects/1/roles
 	url(r'^projects/(?P<role_project>\d+)/roles/$', views.RoleList.as_view(), name='roles'),	
 	# /backend/projects/1/roles
@@ -18,5 +18,5 @@ urlpatterns = patterns('',
 	# /backend/projects
 	url(r'^projects/$', views.ProjectList.as_view(), name='projects'),	
 	# /backend/projects/[proj_name]/
-	url(r'^projects/(?P<pk>\d+)/$', views.ProjectDetail.as_view(), name='project_detail'),	
+	url(r'^projects/(?P<pk>\d+)/$', views.ProjectDetail.as_view(), name='project-detail'),	
 )
